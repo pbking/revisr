27e4c0c88d6f52b1c4b1ca834c32d91b2d1af488
Jason Crist <jcrist@pbking.com>
2023-03-10 10:59:34 -0500

    added icon

--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import { Component } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import { withSelect, withDispatch } from '@wordpress/data';
 import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-site';
-import { blockDefault } from '@wordpress/icons';
 import { Fragment } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
 import apiFetch from '@wordpress/api-fetch';
 import { createReduxStore, register } from '@wordpress/data';
 import { Button, SelectControl, MenuGroup, MenuItem, TextControl } from '@wordpress/components';
-import { store as noticesStore } from '@wordpress/notices';
-import { useDispatch } from '@wordpress/data';
+import { SVG, Path } from '@wordpress/primitives';
 
+const revisrIcon = ( <SVG version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="245.8 381.1 81.9 89.5">
+	<Path d="M295.2,387.2c-5.1,5.1-5.1,13.3,0,18.3c3.8,3.8,9.3,4.7,13.9,2.9l7.2-7.2c1.8-4.7,0.9-10.2-2.9-13.9 C308.5,382.1,300.3,382.1,295.2,387.2z M309.7,401.6c-2.9,2.9-7.6,2.9-10.6,0c-2.9-2.9-2.9-7.6,0-10.6c2.9-2.9,7.6-2.9,10.6,0 C312.6,394,312.6,398.7,309.7,401.6z"/>
+	<Path d="M268.1,454c-13.2-10.1-16.1-29-6.4-42.6c4-5.6,9.4-9.4,15.4-11.4l-2-10.2c-8.5,2.5-16.2,7.7-21.7,15.5 c-12.9,18.2-8.9,43.5,8.8,57l-5.6,8.3l25.9-1.2l-8.6-23.6L268.1,454z"/>
+	<Path d="M318.3,403.3c1.1-2.1,1.7-4.5,1.7-7c0-8.4-6.8-15.2-15.2-15.2s-15.2,6.8-15.2,15.2s6.8,15.2,15.2,15.2 c2.1,0,4.1-0.4,5.9-1.2c8.4,10.6,9.2,25.8,1,37.2c-3.9,5.6-9.4,9.4-15.4,11.4l2,10.2c8.5-2.5,16.2-7.7,21.7-15.5 C331.2,438.1,329.9,417.4,318.3,403.3z M304.8,403.3c-3.8,0-6.9-3.1-6.9-6.9s3.1-6.9,6.9-6.9s6.9,3.1,6.9,6.9 S308.7,403.3,304.8,403.3z"/>
+</SVG>);
 
 const actions = {
 	getInfo() {
@@ -153,7 +156,6 @@ class RevisrPluginComponent extends Component {
 				{ changesMarkup }
 
 				<pre>{JSON.stringify(info, null, ' ')}</pre>
-				<pre>{JSON.stringify(branches, null, ' ')}</pre>
 			</>; 
 
 		}
@@ -161,11 +163,11 @@ class RevisrPluginComponent extends Component {
 		return (
 			<Fragment>
 
-				<PluginSidebarMoreMenuItem target="revisr-sidebar" icon={ blockDefault }>
+				<PluginSidebarMoreMenuItem target="revisr-sidebar" icon={ revisrIcon }>
 					{ __( 'Revisr' ) }
 				</PluginSidebarMoreMenuItem>
 
-				<PluginSidebar name="revisr-sidebar" icon={ blockDefault } title={ __( 'Revisr' ) }>
+				<PluginSidebar name="revisr-sidebar" icon={ revisrIcon } title={ __( 'Revisr' ) }>
 					{ statusMarkup }
 				</PluginSidebar>
 
